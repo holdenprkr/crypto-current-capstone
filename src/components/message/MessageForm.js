11c4fcb043b4b037c28c1a7b35b1c3dfb4c581c7
holdenprkr <holdenprkr@gmail.com>
2020-02-08 15:27:04 -0600

    have add and delete functionality working for message board

--- src/components/message/MessageForm.js
+++ src/components/message/MessageForm.js
@@ -1,13 +1,12 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useState } from "react"
 import { MessageContext } from "./MessageProvider"
 import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form'
 
 
-
-export default ({ message, setMessage, editMode, toggle }) => {
-    const { messages, addMessage, updateMessage } = useContext(MessageContext)
-
+export default () => {
+    const { addMessage } = useContext(MessageContext)
+    const [message, setMessage] = useState({})
 
     const handleControlledInputChange = (event) => {
         const newMessage = Object.assign({}, message)
@@ -15,44 +14,47 @@ export default ({ message, setMessage, editMode, toggle }) => {
         setMessage(newMessage)
     }
 
-    const setDefaults = () => {
-        if (editMode === true) {
-            const messageId = message.id
-            const selectedMessage = messages.find(m => m.id === messageId) || {}
-            setMessage(selectedMessage)
-        }
-    }
-
-    useEffect(() => {
-        setDefaults()
-    }, [message])
-
-    const constructNewMessage = () => {
-        if (editMode) {
-            updateMessage({
-                id: message.id,
-                userId: message.userId,
-                message: message.message,
-                timestamp: new Date(Date.now()).toLocaleDateString('en-US')
-            })
-        } else {
+    const constructNewMessage = () => 
+        {
             addMessage({
-                id: message.id,
-                userId: message.userId,
+                userId: parseInt(localStorage.getItem("activeUser")),
                 message: message.message,
                 timestamp: new Date(Date.now()).toLocaleDateString('en-US')
             })
         }
-    }
 
     return (
         <>
-            <div>
+            <div className="messageInputArea">
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows="3" />
+                    <Form.Control as="textarea" name="message" rows="3" onChange={handleControlledInputChange}/>
                 </Form.Group>
-                <Button type="button" className="btn btn-info publicMessageButton">Add A Public Message</Button>
+                <Button type="button" className="btn btn-info publicMessageButton" onClick={constructNewMessage}>Add A Public Message</Button>
             </div>
         </>
     )
 }
+
+
+    // const setDefaults = () => {
+    //     if (editMode === true) {
+    //         const messageId = message.id
+    //         const selectedMessage = messages.find(m => m.id === messageId) || {}
+    //         setMessage(selectedMessage)
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     setDefaults()
+    // }, [message])
+
+
+
+//         if (editMode) {
+//             updateMessage({
+//                 id: message.id,
+//                 userId: message.userId,
+//                 message: message.message,
+//                 timestamp: new Date(Date.now()).toLocaleDateString('en-US')
+//             })
+//         } else  
